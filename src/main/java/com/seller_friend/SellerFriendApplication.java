3c04c9ba620628faea108558741057ad69cf2d00
이준석 <junseok0508@gmail.com>
2022-12-02 15:15:27 +0900

    221202 소스 커밋

---
+++ src/main/java/com/seller_friend/SellerFriendApplication.java
@@ -0,0 +1,13 @@
+package com.seller_friend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellerFriendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellerFriendApplication.class, args);
+    }
+
+}
